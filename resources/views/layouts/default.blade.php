0b5f578e72c36fdf0ad597d1f17802014d2b02b2
DeteSher <357791771@qq.com>
2022-08-30 14:05:07 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
   </head>
   <body>
 
